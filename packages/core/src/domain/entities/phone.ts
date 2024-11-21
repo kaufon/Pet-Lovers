f5b289d86f7b4b9f32a881726a20b2dd60417c29
kaufon <kauanfvale@gmail.com>
2024-11-21 20:40:48 -0300

    👥entities: add phone

---
+++ packages/core/src/domain/entities/phone.ts
@@ -0,0 +1,16 @@
+import type { PhoneDto } from "../../dto" 
+
+export class Phone {
+  private constructor(
+    readonly ddd: string,
+    readonly number: string,
+  ) {}
+
+  static create(dto: PhoneDto): Phone {
+    return new Phone(dto.ddd, dto.number)
+  }
+
+  get value(): string {
+    return `+${this.ddd} ${this.number}`
+  }
+}
