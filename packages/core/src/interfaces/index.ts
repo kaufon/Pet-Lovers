5d0503232852d4062464ea87c6059a05e8e56910
kaufon <kauanfvale@gmail.com>
2024-11-21 20:42:41 -0300

    📑interfaces: add server app

---
+++ packages/core/src/interfaces/index.ts
@@ -0,0 +1,5 @@
+export type { IServerApp } from "./server-app";
+export * from './repositories'
+export type {IController} from './controller'
+export type { IapiClient } from "./api-client";
+export type { IHttp } from "./IHttp";
