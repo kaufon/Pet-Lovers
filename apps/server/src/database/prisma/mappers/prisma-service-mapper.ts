0010f078e4d54feb3da901faf9f52b9db4f04582
kaufon <kauanfvale@gmail.com>
2024-11-21 20:37:51 -0300

    💾database: add service mapper

---
+++ apps/server/src/database/prisma/mappers/prisma-service-mapper.ts
@@ -0,0 +1,24 @@
+import { Service } from "@core/domain";
+import type { PrismaService } from "../types";
+import { Decimal } from "@prisma/client/runtime/library";
+
+export class PrismaServiceMapper {
+  toDomain(prismaService: PrismaService): Service {
+    return Service.create({
+      id: prismaService.id,
+      name: prismaService.name,
+      price: Number(prismaService.price),
+      type: "SERVICE",
+      ordersCount: prismaService.consumption_number,
+    });
+  }
+  toPrisma(service: Service): PrismaService {
+    return {
+      id: service.id,
+      name: service.name,
+      price: new Decimal(service.price),
+      item_type: "SERVICE",
+      consumption_number: service.ordersCount,
+    };
+  }
+}
