94d6cac84f04267ce0caa28a077f605b6c38d17e
kaufon <kauanfvale@gmail.com>
2024-11-21 20:40:19 -0300

    👥entities: add client

---
+++ packages/core/src/domain/entities/client.ts
@@ -0,0 +1,74 @@
+import type { ClientDto } from "../../dto";
+import { Entity } from "../abstracts";
+import { Cpf } from "./cpf";
+import { Pet } from "./pet";
+import { Phone } from "./phone";
+import { Rg } from "./rg";
+
+type ClientProps = {
+  name: string;
+  socialName: string;
+  cpf: Cpf;
+  pets: Pet[];
+  rgs: Rg[];
+  phones: Phone[];
+};
+
+export class Client extends Entity<ClientProps> {
+  static create(dto: ClientDto) {
+    return new Client(
+      {
+        name: dto.name,
+        socialName: dto.socialName,
+        cpf: Cpf.create(dto.cpf),
+        pets: dto.pets.map(Pet.create),
+        rgs: dto.rgs.map(Rg.create),
+        phones: dto.phones.map(Phone.create),
+      },
+      dto.id,
+    );
+  }
+
+  update(partialDto: Partial<ClientDto>) {
+    return Client.create({
+      ...this.dto,
+      ...partialDto,
+    });
+  }
+
+  get name(): string {
+    return this.props.name;
+  }
+
+  get socialName(): string {
+    return this.props.socialName;
+  }
+
+  get cpf(): Cpf {
+    return this.props.cpf;
+  }
+
+  get pets(): Pet[] {
+    return this.props.pets;
+  }
+
+  get rgs(): Rg[] {
+    return this.props.rgs;
+  }
+
+  get phones(): Phone[] {
+    return this.props.phones;
+  }
+  get dto(): ClientDto {
+    return {
+      id: this.id,
+      name: this.props.name,
+      socialName: this.props.socialName,
+      cpf: this.props.cpf,
+      pets: this.props.pets,
+      rgs: this.props.rgs,
+      phones: this.props.phones,
+    };
+  }
+}
+
