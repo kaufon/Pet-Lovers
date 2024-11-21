1b18b349fd16d93683c456db4f8e89a751ea76ca
kaufon <kauanfvale@gmail.com>
2024-11-21 20:37:45 -0300

    💾database: add product mapper

---
+++ apps/server/src/database/prisma/mappers/prisma-product-mapper.ts
@@ -0,0 +1,23 @@
+import { Product } from "@core/domain";
+import type { PrismaProduct } from "../types";
+import { Decimal } from "@prisma/client/runtime/library";
+
+export class PrismaProductsMapper {
+  toDomain(prismaProduct: PrismaProduct): Product {
+    return Product.create({
+      id: prismaProduct.id,
+      name: prismaProduct.name,
+      price: Number(prismaProduct.price),
+      type: "PRODUCT",
+    });
+  }
+  toPrisma(product: Product): PrismaProduct {
+    return {
+      id: product.id,
+      name: product.name,
+      item_type: "PRODUCT",
+      price: new Decimal(product.price),
+      consumption_number: product.ordersCount,
+    };
+  }
+}
