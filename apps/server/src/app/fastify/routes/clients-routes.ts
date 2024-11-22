ae0b883b3b69bfe7997af8befd221c82139ec162
kaufon <kauanfvale@gmail.com>
2024-11-22 20:47:45 -0300

    🛤️routes:clietns routes

---
+++ apps/server/src/app/fastify/routes/clients-routes.ts
@@ -0,0 +1,36 @@
+import { DeleteClientController, ListClientController, ListTopConsumersControllers, ListTopSpenderController, RegisterClientController, UpdateClientController } from "apps/server/src/api/controllers";
+import type { FastifyInstance } from "fastify";
+import { FastifyHttp } from "../fastify-http";
+
+export const ClientsRoutes = async (app:FastifyInstance) => {
+  const listClientController = new ListClientController()
+  const registerClientcontroller = new RegisterClientController()
+  const deleteClientcontroller = new DeleteClientController()
+  const updateClientcontroller = new UpdateClientController()
+  const listtopConsumerscontrller = new ListTopConsumersControllers()
+  const listTopSpendersController = new ListTopSpenderController()
+  app.get("/",async (request,response) => {
+    const http = new FastifyHttp(request,response)
+    return listClientController.handle(http)
+  })
+  app.get("/consumers",async (request,response) => {
+  const http = new FastifyHttp(request,response)    
+    return listtopConsumerscontrller.handle(http)
+  })
+  app.get("/spenders",async (request,response) => {
+  const http = new FastifyHttp(request,response)    
+  return listTopSpendersController.handle(http)
+  })
+  app.post("/", async (request,response) =>{
+    const http = new FastifyHttp(request,response)
+    return registerClientcontroller.handle(http)
+  })
+  app.delete("/",async (request,response)=>{
+    const http = new FastifyHttp(request,response)
+    return deleteClientcontroller.handle(http)
+  })
+  app.put("/:clientId",async (request,response) => {
+    const http = new FastifyHttp(request,response)
+    return updateClientcontroller.handle(http)
+  })
+}
