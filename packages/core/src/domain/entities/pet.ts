b1a7383191ac3e2b1038d36f5e305937e65b6157
kaufon <kauanfvale@gmail.com>
2024-11-22 20:38:48 -0300

    🐛fix: fix pet entity

--- packages/core/src/domain/entities/pet.ts
+++ packages/core/src/domain/entities/pet.ts
@@ -2,55 +2,55 @@ import type { PetDto } from "../../dto";
 import { Entity } from "../abstracts";
 
 type PetProps = {
-  name: string;
-  type: string;
-  race: string;
-  gender: string;
+	name: string;
+	type: string;
+	race: string;
+	gender: string;
 };
 
 export class Pet extends Entity<PetProps> {
-  static create(dto: PetDto) {
-    return new Pet(
-      {
-        name: dto.name,
-        type: dto.type,
-        race: dto.race,
-        gender: dto.gender,
-      },
-      dto.id,
-    );
-  }
+	static create(dto: PetDto) {
+		return new Pet(
+			{
+				name: dto.name,
+				type: dto.type,
+				race: dto.race,
+				gender: dto.gender,
+			},
+			dto.id,
+		);
+	}
 
-  update(partialDto: Partial<PetDto>) {
-    return Pet.create({
-      ...this.dto,
-      ...partialDto,
-    });
-  }
+	update(partialDto: Partial<PetDto>) {
+		return Pet.create({
+			...this.dto,
+			...partialDto,
+		});
+	}
 
-  get name(): string {
-    return this.props.name;
-  }
+	get name(): string {
+		return this.props.name;
+	}
 
-  get type(): string {
-    return this.props.type;
-  }
+	get type(): string {
+		return this.props.type;
+	}
 
-  get race(): string {
-    return this.props.race;
-  }
+	get race(): string {
+		return this.props.race;
+	}
 
-  get gender(): string {
-    return this.props.gender;
-  }
+	get gender(): string {
+		return this.props.gender;
+	}
 
-  get dto(): PetDto {
-    return {
-      id: this.id,
-      name: this.props.name,
-      type: this.props.type,
-      race: this.props.race,
-      gender: this.props.gender,
-    };
-  }
+	get dto(): PetDto {
+		return {
+			id: this.id,
+			name: this.props.name,
+			type: this.props.type,
+			race: this.props.race,
+			gender: this.props.gender,
+		};
+	}
 }
