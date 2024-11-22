33d3635b9ebbfff02acb0a45615b6e6d0b4cdc1f
kaufon <kauanfvale@gmail.com>
2024-11-22 20:38:12 -0300

    🐛fix:fix consumption entity

--- packages/core/src/domain/entities/consumption.ts
+++ packages/core/src/domain/entities/consumption.ts
@@ -1,29 +1,47 @@
+import { ConsumptionDto } from "../../dto";
+import { Entity } from "../abstracts";
+
 type ConsumptionProps = {
+  id?: string;
   clientId: string;
   itemId: string;
   amount: number;
+  price?: number;
 };
-export class Consumption {
-  readonly clientId: string;
-  readonly itemId: string;
-  readonly amount: number;
-  private constructor(props: ConsumptionProps) {
-    this.clientId = props.clientId;
-    this.itemId = props.itemId;
-    this.amount = props.amount;
-  }
+export class Consumption extends Entity<ConsumptionProps> {
   static create(dto: ConsumptionProps) {
-    return new Consumption({
-      amount: dto.amount,
-      itemId: dto.itemId,
-      clientId: dto.clientId,
-    });
+    return new Consumption(
+      {
+        amount: dto.amount,
+        itemId: dto.itemId,
+        clientId: dto.clientId,
+        price: dto.price,
+      },
+      dto.id,
+    );
+  }
+  update(dto: Partial<ConsumptionDto>): Consumption {
+    return Consumption.create({ ...this.dto, ...dto });
+  }
+  get amount(): number {
+    return this.props.amount;
+  }
+  get price(): number {
+    return this.props.price || 0;
+  }
+  get itemId(): string {
+    return this.props.itemId;
+  }
+  get clientId(): string {
+    return this.props.clientId;
   }
   get dto() {
     return {
-      clientID: this.clientId,
-      amount: this.amount,
-      itemId: this.itemId,
+      id: this.id,
+      clientId: this.props.clientId,
+      amount: this.props.amount,
+      itemId: this.props.itemId,
+      price: this.props.price,
     };
   }
 }
