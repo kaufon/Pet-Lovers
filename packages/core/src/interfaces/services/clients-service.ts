ebb72eaef8347ce75b2c92a7d190d1f3df748038
kaufon <kauanfvale@gmail.com>
2024-11-27 22:00:32 -0300

    r♻️efactor: clients interface

--- packages/core/src/interfaces/services/clients-service.ts
+++ packages/core/src/interfaces/services/clients-service.ts
@@ -1,11 +1,61 @@
-
 import type { ClientDto } from "../../dto";
 import type { ApiResponse, PaginationResponse } from "../../responses";
 
 export interface IClientsService {
+  getClientDetails(clientId: string): Promise<
+    ApiResponse<{
+      client: ClientDto;
+      consumptions: {
+        itemId: string;
+        itemName: string;
+        totalAmount: number;
+        totalSpent: number;
+      }[];
+    }>
+  >;
   listClients(): Promise<ApiResponse<PaginationResponse<ClientDto>>>;
-  updateClient(partialClientDto: Partial<ClientDto>, clientId: string): Promise<ApiResponse<void>>;
+  updateClient(
+    partialClientDto: Partial<ClientDto>,
+    clientId: string,
+  ): Promise<ApiResponse<void>>;
   deleteClient(clientId: string): Promise<ApiResponse<void>>;
   registerClient(client: ClientDto): Promise<ApiResponse<void>>;
+  listSpenders(): Promise<
+    ApiResponse<
+      PaginationResponse<{
+        clientId: string;
+        name: string;
+        totalSpended: number;
+      }>
+    >
+  >;
+  listConsumers(): Promise<
+    ApiResponse<
+      PaginationResponse<{ clientId: string; name: string; totalItems: number }>
+    >
+  >;
+  findTop10ConsumedItemsByType(): Promise<
+    ApiResponse<
+      PaginationResponse<
+        {
+          petType: string;
+          productName: string;
+          totalAmount: number;
+          itemId: string;
+        }[]
+      >
+    >
+  >;
+  findTop10ConsumedItemsByRace(): Promise<
+    ApiResponse<
+      PaginationResponse<
+        {
+          petRace: string;
+          productName: string;
+          totalAmount: number;
+          itemId: string;
+        }[]
+      >
+    >
+  >;
 }
-
--- packages/core/src/interfaces/services/index.ts
+++ packages/core/src/interfaces/services/index.ts
@@ -1,3 +1,4 @@
 export type {IProductsService} from "./products-service"
+export type {IConsumptionsService} from "./consumptions-service"
 export type {IClientsService} from "./clients-service"
 export type {IServicesService} from "./services-service"
