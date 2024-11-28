8a3886165468e6b989be3274791ab648057ad616
kaufon <kauanfvale@gmail.com>
2024-11-27 21:59:52 -0300

    ♻️refactor: clients service

--- apps/web/src/api/services/client-service.ts
+++ apps/web/src/api/services/client-service.ts
@@ -1,13 +1,38 @@
-import type {
+import {
   IClientsService,
   IapiClient,
   PaginationResponse,
   ApiResponse,
   ClientDto,
-} from "@core"; // Adjust based on your project structure
+} from "@core";
+import { m } from "framer-motion";
 
 export const ClientsService = (apiClient: IapiClient): IClientsService => {
   return {
+    async listConsumers() {
+      return await apiClient.get("/clients/consumers");
+    },
+    async listSpenders() {
+      return await apiClient.get("/clients/spenders");
+    },
+    async findTop10ConsumedItemsByType() {
+      return await apiClient.get("/clients/pet/type");
+    },
+    async findTop10ConsumedItemsByRace() {
+      return await apiClient.get("/clients/pet/race");
+    },
+
+    async getClientDetails(clientId) {
+      return await apiClient.get<{
+        client: ClientDto;
+        consumptions: {
+          itemId: string;
+          itemName: string;
+          totalAmount: number;
+          totalSpent: number;
+        }[];
+      }>(`/clients/${clientId}`);
+    },
     async listClients() {
       return await apiClient.get<PaginationResponse<ClientDto>>("/clients");
     },
